91e3b5bdd1627dd3b724fb5e8cfe2c3a9b41cb07
VG <vg@email.com>
2022-09-26 21:40:18 +1000

    Part2_01.Squared

--- part02-Part02_01.Squared/src/main/java/Squared.java
+++ part02-Part02_01.Squared/src/main/java/Squared.java
@@ -5,6 +5,11 @@ public class Squared {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        
+        int number = Integer.valueOf(scanner.nextLine());
+        
+        int square = number * number;
+        
+        System.out.println(square);
     }
 }
