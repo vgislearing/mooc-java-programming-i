3942a44c45d5425d3099697e1e091e106656afcb
VG <vg@email.com>
2022-09-26 22:06:17 +1000

    Part2_04.ComparingNumbers

--- part02-Part02_04.ComparingNumbers/src/main/java/ComparingNumbers.java
+++ part02-Part02_04.ComparingNumbers/src/main/java/ComparingNumbers.java
@@ -5,6 +5,15 @@ public class ComparingNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        int one = Integer.valueOf(scanner.nextLine());
+        int two = Integer.valueOf(scanner.nextLine());
+        
+        if (one == two) {
+            System.out.println(one + " is equal to" + two + ".");
+        } else if (one > two) {
+            System.out.println(one + " is greater than " + two + ".");
+        } else if (one < two) {
+            System.out.println(one + " is smaller than " + two + ".");
+        } 
     }
 }
